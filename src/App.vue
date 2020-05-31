44f576a43009d43f299640435959d460239bf20d
Martijn Dorsman <martijn.dorsman@gmail.com>
2020-05-31 21:51:55 +0200

    Basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
-  <div class="container mx-auto">nasa-picture</div>
+  <div class="container mx-auto" v-if="data">
+    <h2>{{ data.title }}</h2>
+    <img :src="data.hdurl" />
+    {{data.explanation}}
+  </div>
 </template>
-
+ 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent, ref, onMounted } from "vue"
 
 export default defineComponent({
   name: "App",
-  components: {}
+  components: {},
+
+  setup() {
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
+
+    const data = ref({})
+    onMounted(() => {
+      fetch(url)
+        .then(stream => stream.json())
+        .then(resp => {
+          data.value = resp
+        })
+    })
+
+    return {
+      data
+    }
+  }
 })
 </script>
